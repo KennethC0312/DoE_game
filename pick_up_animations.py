572ec82e8ad3c19c2e441c1d637738a0660416ee
KennethC0312 <135835299+KennethC0312@users.noreply.github.com>
2024-01-19 13:25:43 +0000

    wow

--- pick_up_animations.py
+++ pick_up_animations.py
@@ -33,6 +33,49 @@ pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/
 pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/pick_up_confirmation', 'pick_up_18.png')), (screen_size, screen_size)),
 pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/pick_up_confirmation', 'pick_up_19.png')), (screen_size, screen_size)),
 pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/pick_up_confirmation', 'pick_up_20.png')), (screen_size, screen_size))]
+    self.wow = [pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'loop_1.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'loop_2.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'Wow_1.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'Wow_2.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'Wow_3.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'Wow_4.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'Wow_5.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'Wow_6.png')), (screen_size, screen_size)),
+       pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/wow', 'Wow_7.png')), (screen_size, screen_size))]
+    self.got = [pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'loop_1.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'loop_2.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_1.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_2.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_3.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_4.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_5.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_6.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_7.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_8.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_9.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_10.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_11.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_12.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_13.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_14.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_15.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_16.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_17.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_18.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_19.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_20.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_21.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_22.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_23.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_24.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_25.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_26.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_27.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_28.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_29.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_30.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_31.png')), (screen_size, screen_size)),
+               pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/you_got', 'You_got_32.png')), (screen_size, screen_size))]
     self.numbers = [scale(pygame.image.load(os.path.join('assets/number', '0.png')),(34,48)),
                    scale(pygame.image.load(os.path.join('assets/number', '1.png')),(34,48)),
                    scale(pygame.image.load(os.path.join('assets/number', '2.png')),(34,48)),
@@ -88,7 +131,7 @@ pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/
                    pygame.transform.scale(pygame.image.load(os.path.join('assets/got_gun/no more dig for you', 'no_more_dig_18.png')), (screen_size, 150)),
                    pygame.transform.scale(pygame.image.load(os.path.join('assets/got_gun/no more dig for you', 'no_more_dig_19.png')), (screen_size, 150)),]
     self.identify = [pygame.transform.scale(pygame.image.load(os.path.join('assets/others', 'armour.png')), (250, 250)),
-                     pygame.transform.scale(pygame.image.load(os.path.join('assets/others', 'sword.png' )), (250, 250)),]
+                     pygame.transform.scale(pygame.image.load(os.path.join('assets/others', 'sword.png' )), (250, 250))]
     self.jump = [180, 190, 200, 210, 220, 230, 230, 230, 220, 210, 200, 190, 180, 180]
     self.torch_level = torch_level
     self.clock = pygame.time.Clock()
@@ -99,11 +142,65 @@ pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/
     self.clip = pygame.Rect(0, 0, screen_size, screen_size)
     self.screen = screen
 
-
   def pick_up_animation(self):
     self.clock.tick(200)
     y = 0
+    n = 1
+    identify_x = 200
+    identify_y = 150
+    for i in range(len(self.wow)-2):
+      self.sc.blit(self.wow[i+2], (0,0))
+      self.screen.set_clip(self.clip)
+      self.screen.blit(self.sc, self.clip)
+      pygame.display.flip()
+      time.sleep(0.2)
+    while not self.clicked:
+      n = 1 if n == 0 else 0
+      self.sc.blit(self.wow[n], (0,0))
+      self.screen.set_clip(self.clip)
+      self.screen.blit(self.sc, self.clip)
+      for events in pygame.event.get():
+        pygame.display.flip()
+        if events.type == pygame.MOUSEBUTTONDOWN:
+          mouse_presses = pygame.mouse.get_pressed()
+          if mouse_presses[0]:
+            self.clicked = True
+      pygame.display.flip()
+      time.sleep(0.2)
+    n = 1
+    self.clicked = False
     if self.type == 'armour':
+      for i in range(len(self.got)-2):
+        self.sc.blit(self.got[i+2], (0,0))
+        self.sc.blit(self.identify[0], (200, 150))
+        self.screen.set_clip(self.clip)
+        self.screen.blit(self.sc, self.clip)
+        pygame.display.flip()
+        time.sleep(0.2)
+      while not self.clicked:
+        n = 1 if n == 0 else 0
+        self.sc.blit(self.got[n], (0,0))
+        self.sc.blit(self.identify[0], (identify_x, identify_y))
+        self.screen.set_clip(self.clip)
+        self.screen.blit(self.sc, self.clip)
+        for events in pygame.event.get():
+          pygame.display.flip()
+          if events.type == pygame.MOUSEBUTTONDOWN:
+            mouse_presses = pygame.mouse.get_pressed()
+            if mouse_presses[0]:
+              self.clicked = True
+        pygame.display.flip()
+        time.sleep(0.2)
+      self.clicked = False
+      for i in range(3):
+        identify_x += 63
+        identify_y += 10
+        self.sc.blit(self.got[n], (0,0))
+        self.sc.blit(self.identify[0], (identify_x, identify_y))
+        self.screen.set_clip(self.clip)
+        self.screen.blit(self.sc, self.clip)
+        pygame.display.flip()
+        time.sleep(0.2)
       for i in range(len(self.pick_up_confirmation)):
         y = (y+1) if y != len(self.jump) else 0
         self.sc.blit(self.pick_up_confirmation[i], (0,0))
@@ -143,6 +240,13 @@ pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/
         self.screen.blit(self.sc, self.clip)
         pygame.display.flip()
     elif self.type == 'weapon':
+      for i in range(len(self.got)-1):
+        self.sc.blit(self.got[i+2], (0,0))
+        self.sc.blit(self.identify[0], (200, 150))
+        self.screen.set_clip(self.clip)
+        self.screen.blit(self.sc, self.clip)
+        pygame.display.flip()
+        time.sleep(0.2)
       for i in range(len(self.pick_up_confirmation)):
         y = (y+1) if y != len(self.jump) else 0
         self.sc.blit(self.pick_up_confirmation[i], (0,0))
@@ -153,6 +257,15 @@ pygame.transform.scale(pygame.image.load(os.path.join('assets/pick_up_animation/
         for i in range(len(self.new_stat)):
           self.sc.blit(self.numbers[int(self.new_stat[i])], self.coods2[i])
         self.sc.blit(self.identify[1], (390, self.jump[y-1]))
+        self.screen.set_clip(self.clip)
+        self.screen.blit(self.sc, self.clip)
+        pygame.display.flip()
+        time.sleep(0.2)
+      for i in range(3):
+        identify_x += 63
+        identify_y += 10
+        self.sc.blit(self.got[n], (0,0))
+        self.sc.blit(self.identify[0], (identify_x, identify_y))
         self.screen.set_clip(self.clip)
         self.screen.blit(self.sc, self.clip)
         pygame.display.flip()
